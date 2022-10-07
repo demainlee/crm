97e174e439e8e594e404cfa115dd589fa46471bf
demain_lee <lmm_work@163.com>
2022-10-07 13:49:53 +0800

    :tada: first commit

---
+++ src/main/java/top/upstudy/crm/service/impl/test.java
@@ -0,0 +1,11 @@
+package top.upstudy.crm.service.impl;
+
+import top.upstudy.crm.utils.Md5Util;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println(Md5Util.encode("123456"));
+    }
+}
